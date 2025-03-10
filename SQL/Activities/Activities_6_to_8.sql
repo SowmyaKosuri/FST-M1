45c8c6700fcf9b9e5ffd8ccc170566030de3841f
SowmyaKosuri <kosuri.mani.sowmya@ibm.com>
2025-03-10 09:04:18 +0530

    Activities_6_to_8.sql

--- SQL/Activities/Activities_6_to_8.sql
+++ SQL/Activities/Activities_6_to_8.sql
@@ -1,9 +1,12 @@
--- ACTIVITY 6
--- Creating a table named orders
+--ACTIVITY 6---------
+
+-- Create a table named orders
 create table orders(
     order_no int primary key, purchase_amount float, order_date date,
-    customer_id int, salesmen_id int);
--- Adding values to the table
+    customer_id int, salesman_id int);
+
+-- Add values to the table
+
 INSERT ALL
     INTO orders VALUES(70001, 150.5, TO_DATE('2012/10/05', 'YYYY/MM/DD'), 3005, 5002) 
     INTO orders VALUES(70009, 270.65, TO_DATE('2012/09/10', 'YYYY/MM/DD'), 3001, 5005)
@@ -18,31 +21,50 @@ INSERT ALL
     INTO orders VALUES(70011, 75.29, TO_DATE('2012/08/17', 'YYYY/MM/DD'), 3003, 5007)
     INTO orders VALUES(70013, 3045.6, TO_DATE('2012/04/25', 'YYYY/MM/DD'), 3002, 5001)
 SELECT 1 FROM DUAL;
-DESCRIBE orders;
+
 -- Get all salesman ids without any repeated values
-select distinct salesmen_id from orders;
+SELECT distinct salesman_id from orders;
+
 -- Display the order number ordered by date in ascending order
-select order_no, order_date from orders order by order_date;
+SELECT order_no, order_date from orders order by order_date;
+
 -- Display the order number ordered by purchase amount in descending order
-select order_no, purchase_amount from orders order by purchase_amount DESC;
+SELECT order_no, purchase_amount from orders order by purchase_amount DESC;
+
 -- Display the full data of orders that have purchase amount less than 500.
-select * from orders where purchase_amount < 500;
+SELECT * from orders where purchase_amount < 500;
+
 -- Display the full data of orders that have purchase amount between 1000 and 2000.
-select * from orders where purchase_amount between 1000 and 2000;
+SELECT * from orders where purchase_amount between 1000 and 2000;
 
--- ACTIVITY 7
+
+--ACTIVITY 7-------
+
+-- Write an SQL statement to find the total purchase amount of all orders.
 select SUM(purchase_amount) AS "Total sum" from orders;
-select AVG(purchase_amount) AS "Average" from orders;
-select MAX(purchase_amount) AS "Maximum" from orders;
-select MIN(purchase_amount) AS "Minumum" from orders;
-select COUNT(distinct salesmen_id) AS "Total count" from orders;
 
---ACTIVITY 8
+-- Write an SQL statement to find the average purchase amount of all orders.
+select AVG(purchase_amount) AS "Average" from orders;
+
+-- Write an SQL statement to get the maximum purchase amount of all the orders.
+select MAX(purchase_amount) AS "Maximum" from orders;
+
+-- Write an SQL statement to get the minimum purchase amount of all the orders.
+select MIN(purchase_amount) AS "Minumum" from orders;
+
+-- Write an SQL statement to find the number of salesmen listed in the table.
+select COUNT(distinct salesman_id) AS "Total count" from orders;
+
+
+--ACTIVITY 8 -----------
+
 -- Write an SQL statement to find the highest purchase amount ordered by the each customer with their ID and highest purchase amount.
 SELECT customer_id, MAX(purchase_amount) AS "Max Amount" FROM orders GROUP BY customer_id;
+
 -- Write an SQL statement to find the highest purchase amount on '2012-08-17' for each salesman with their ID.
-SELECT salesmen_id, order_date, MAX(purchase_amount) AS "Max Amount" FROM orders 
-WHERE order_date=To_DATE('2012/08/17', 'YYYY/MM/DD') GROUP BY salesmen_id, order_date;
+SELECT salesman_id, order_date, MAX(purchase_amount) AS "Max Amount" FROM orders 
+WHERE order_date=To_DATE('2012/08/17', 'YYYY/MM/DD') GROUP BY salesman_id, order_date;
+
 -- Write an SQL statement to find the highest purchase amount with their ID and order date, for only those customers who have a higher purchase amount within the list 2030, 3450, 5760, and 6000.
 SELECT customer_id, order_date, MAX(purchase_amount) AS "Max Amount" FROM orders
 GROUP BY customer_id, order_date
